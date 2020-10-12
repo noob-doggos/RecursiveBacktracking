06ab13e27ed6180ca482df3c1a9e0f07c3906396
Doki <duvictor514@gmail.com>
2020-10-12 15:46:30 -0700

    Added comments

--- src/RecursiveBacktrackingMain.java
+++ src/RecursiveBacktrackingMain.java
@@ -10,6 +10,7 @@ public class RecursiveBacktrackingMain
 
     public static String travelHelper(int x, int y, String pathCur)
     {
+        // check base cases to return on.
         if (x == 0 && y == 0)
         {
             return pathCur + "\n";
@@ -25,6 +26,8 @@ public class RecursiveBacktrackingMain
             return pathCur + "E\n";
         }
 
+        // try branching our paths.
+        // we can start going northeast, east or north.
         String path = "";
 
         if (x > 0 && y > 0)
@@ -56,12 +59,15 @@ public class RecursiveBacktrackingMain
 
     public static void countBinaryHelper(int i, int n)
     {
+        // base case: if n = 0, print only a blank line and return.
         if (n == 0)
         {
             System.out.println();
             return;
         }
 
+        // hacky code using string manipulation to convert base-10 ints i and
+        // 2^(n-1) to string with base-2 equivalent padded with zeroes
         int maxNumSpaces = Integer.toBinaryString((int) Math.pow(2, n - 1)).length();
         String targetStr = String.format("%" + maxNumSpaces + "s", "").replaceAll(" ", "1");
         String binaryNum = Integer.parseInt(Integer.toBinaryString(i)) + "";
@@ -69,11 +75,14 @@ public class RecursiveBacktrackingMain
 
         System.out.println(binaryNumFormatted);
 
+        // check if our current binary value equals the maximum binary value for
+        // n bits. if so, return
         if (binaryNum.equals(targetStr))
         {
             return;
         }
 
+        // print the next binary value incrementally
         countBinaryHelper(i + 1, n);
     }
 
@@ -88,18 +97,26 @@ public class RecursiveBacktrackingMain
 
     public static void maxSumHelper(List<Integer> L, int index, int limit, int sum, List<Integer> owo)
     {
+        // toss out this instance if our sum has exceeded the limit, as there
+        // are no negative numbers in the list
         if (sum > limit)
         {
             return;
         }
 
+        // add the current sum to the list of legal sums.
         owo.add(sum);
 
+        // if our index is out of the bounds of our list L, return to prevent
+        // NPE.
         if (index >= L.size())
         {
             return;
         }
 
+        // spawn two more instances of this method, one where we add the current
+        // list value, one where we don't to cover all possible combinations of
+        // list elements. worst-case time complexity O(n^2)
         int num = L.get(index);
         maxSumHelper(L, index + 1, limit, sum + num, owo);
         maxSumHelper(L, index + 1, limit, sum, owo);
@@ -124,11 +141,16 @@ public class RecursiveBacktrackingMain
 
     public static boolean partitionableHelper(List<Integer> list, int index, int sum1, int sum2)
     {
+        // if we have summed up all sub-lists within our list, we check whether
+        // our left array sum == right array sum
         if (index == list.size())
         {
             return sum1 == sum2;
         }
-        
+
+        // spawn two more instances of this method, one where we add the current
+        // element to the left sum, other one where we leave it out of the left
+        // sum and add to right sum instead.
         return partitionableHelper(list, index + 1, sum1 + list.get(index), sum2)
             || partitionableHelper(list, index + 1, sum1, sum2 + list.get(index));
     }
