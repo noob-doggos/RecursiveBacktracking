4e4f6b603a6772b955e009b52dac6149428adc5b
Doki <duvictor514@gmail.com>
2020-10-12 12:47:14 -0700

    Added travel solution

---
+++ src/RecursiveBacktrackingMain.java
@@ -0,0 +1,51 @@
+
+public class RecursiveBacktrackingMain
+{
+    public static String travelHelper(int x, int y, String pathCur)
+    {
+        if (x == 0 && y == 0)
+        {
+            return pathCur + "\n";
+        }
+
+        if (x == 0 && y == 1)
+        {
+            return pathCur + "N\n";
+        }
+
+        if (x == 1 && y == 0)
+        {
+            return pathCur + "E\n";
+        }
+
+        String path = "";
+
+        if (x > 0 && y > 0)
+        {
+            path += travelHelper(x - 1, y - 1, pathCur + "NE ");
+        }
+
+        if (x > 0)
+        {
+            path += travelHelper(x - 1, y, pathCur + "E ");
+        }
+
+        if (y > 0)
+        {
+            path += travelHelper(x, y - 1, pathCur + "N ");
+        }
+
+        return path;
+    }
+
+    public static void travel(int x, int y)
+    {
+        travelHelper(x, y, "");
+    }
+
+    public static void main(String[] args)
+    {
+        travel(2, 2);
+    }
+
+}
