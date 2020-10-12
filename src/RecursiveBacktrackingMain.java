5ebde8063b3cc36e235472d5508a51c23724c083
Doki <duvictor514@gmail.com>
2020-10-12 13:58:34 -0700

    Added countBinary solution

--- src/RecursiveBacktrackingMain.java
+++ src/RecursiveBacktrackingMain.java
@@ -1,6 +1,10 @@
 
 public class RecursiveBacktrackingMain
 {
+    //
+    // Response 1: travel
+    //
+
     public static String travelHelper(int x, int y, String pathCur)
     {
         if (x == 0 && y == 0)
@@ -40,12 +44,45 @@ public class RecursiveBacktrackingMain
 
     public static void travel(int x, int y)
     {
-        System.out.println(travelHelper(x, y, ""));
+        System.out.print(travelHelper(x, y, ""));
+    }
+
+    //
+    // Response 2: countBinary
+    //
+
+    public static void countBinaryHelper(int i, int n)
+    {
+        if (n == 0)
+        {
+            System.out.println();
+            return;
+        }
+
+        int maxNumSpaces = Integer.toBinaryString((int) Math.pow(2, n - 1)).length();
+        String targetStr = String.format("%" + maxNumSpaces + "s", "").replaceAll(" ", "1");
+        String binaryNum = Integer.parseInt(Integer.toBinaryString(i)) + "";
+        String binaryNumFormatted = String.format("%" + maxNumSpaces + "s", binaryNum + "").replaceAll(" ", "0");
+
+        System.out.println(binaryNumFormatted);
+
+        if (binaryNum.equals(targetStr))
+        {
+            return;
+        }
+
+        countBinaryHelper(i + 1, n);
+    }
+
+    public static void countBinary(int n)
+    {
+        countBinaryHelper(0, n);
     }
 
     public static void main(String[] args)
     {
         travel(2, 2);
+        countBinary(5);
     }
 
 }
